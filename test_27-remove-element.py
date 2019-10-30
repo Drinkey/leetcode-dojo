525d7638d4e4b2aae744328a17c35a98cbde4a40
Junkai Zhang <junkai.zhang@watchguard.com>
2019-10-30 10:49:13 +0800

    add: 27 remove element solution

--- test_27-remove-element.py
+++ test_27-remove-element.py
@@ -2,30 +2,40 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        pass
+        len_of_nums = len(nums)
+        j = 0 # index of new nums-in-place
+        for i in range(len_of_nums):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j+=1
+        return j
 
 s = Solution()
 
 def test_sample_1():
     nums = [3,2,2,3]
     val = 3
-    assert s.removeElement(nums, val) == 2
-    assert nums == [2,2]
+    r = s.removeElement(nums, val)
+    assert r == 2
+    assert nums[:r] == [2,2]
 
 def test_sample_2():
     nums = [0,1,2,2,3,0,4,2]
     val = 2
-    assert s.removeElement(nums, val) == 5
-    assert nums == [0, 1, 3, 0, 4]
+    r = s.removeElement(nums, val)
+    assert r == 5
+    assert nums[:r] == [0, 1, 3, 0, 4]
 
 def test_val_not_in_nums():
     nums = [0,1,2,2,3,0,4,2]
     val = 7
-    assert s.removeElement(nums, val) == 8
-    assert nums == [0,1,2,2,3,0,4,2]
+    r = s.removeElement(nums, val)
+    assert r == 8
+    assert nums[:r] == [0,1,2,2,3,0,4,2]
 
 def test_nums_all_is_val():
     nums = [110, 110, 110, 110, 110, 110, 110]
     val = 110
-    assert s.removeElement(nums, val) == 7
+    r = s.removeElement(nums, val)
+    assert r == 0
 
